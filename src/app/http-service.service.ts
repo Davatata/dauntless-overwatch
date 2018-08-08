e68903603c39cce7d0ab840e2a8f7500aa4473f0
David <Davatata@users.noreply.github.com>
2018-08-07 21:02:05 -0700

    small changes

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class HttpServiceService {
--- src/app/player-stats/player-stats.component.ts
+++ src/app/player-stats/player-stats.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { HttpServiceService } from '../http-service.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-player-stats',
